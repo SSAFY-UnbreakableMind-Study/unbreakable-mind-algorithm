6595c509fb2462d50e94f575e0ad5ca02a6b32e1
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-01-15 12:56:01 +0900

    Revert "6"

--- 유승민/week0/BOJ_9251_G5_LCS.cpp
+++ 유승민/week0/BOJ_9251_G5_LCS.cpp
@@ -4,13 +4,19 @@
 using namespace std;
 using int64 = int64_t;
 
-string a,b;
-
 int main() {
 	fastio;
-	
-	cin>>a>>b;
-	
+	int64 N, M, dp[101] = { 0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12, 16, 21, 28, 37, 49 };
+	cin >> N;
+
+	for (int64 i = 17; i < 101; ++i) {
+		dp[i] = dp[i - 1] + dp[i - 5];
+	}
+
+	for (int64 i = 0; i < N; ++i) {
+		cin >> M;
+		cout << dp[M] << "\n";
+	}
 
 	return EXIT_SUCCESS;
 }
