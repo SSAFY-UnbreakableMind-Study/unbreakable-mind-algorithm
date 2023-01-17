9a24e79abee34dd17aa0ab693720b721b2582e7a
고우민 <kwm907@gmail.com>
2023-01-17 17:58:38 +0900

    고우민 : BOJ_9461_S3_파도반수열_ADD

--- 고우민/week0/BOJ_9461_S3_파도반수열.java
+++ 고우민/week0/BOJ_9461_S3_파도반수열.java
@@ -1,16 +1,18 @@
-public class BOJ_9461_S3_파도반수열{
-    public static void main(String[] args){
+import java.util.Scanner;
+
+public class BOJ_9461_S3_파도반수열 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         int[] dp = new int[101];
-        for(int i=1;i<3;i++){
+        for (int i = 1; i < 4; i++) {
             dp[i] = 1;
         }
-        dp[3] = dp[4] = 2;
-        for(int i=5;i<101;i++){
-            dp[i] = dp[i-1] + dp[i-5];
+        dp[4] = dp[5] = 2;
+        for (int i = 6; i < 101; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
         }
-        for(int t=1;t<=T;t++){
+        for (int t = 1; t <= T; t++) {
             int n = sc.nextInt();
             System.out.println(dp[n]);
         }
