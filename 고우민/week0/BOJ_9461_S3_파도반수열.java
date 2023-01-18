0686690e078185faf52c3dd530926b763d3ac25e
고우민 <kwm907@gmail.com>
2023-01-18 11:06:05 +0900

    고우민 : BOJ_9461_S3_파도반수열_Add

--- 고우민/week0/BOJ_9461_S3_파도반수열.java
+++ 고우민/week0/BOJ_9461_S3_파도반수열.java
@@ -5,10 +5,12 @@ public class BOJ_9461_S3_파도반수열 {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         long[] dp = new long[101];
+
         for (int i = 1; i < 4; i++) {
             dp[i] = 1;
         }
         dp[4] = dp[5] = 2;
+
         for (int i = 6; i < 101; i++) {
             dp[i] = dp[i - 1] + dp[i - 5];
         }
