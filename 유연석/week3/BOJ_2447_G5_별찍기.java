9c16fdf11f061617af6c81e8a460a9f20c8c6b03
유연석 <ysyoo627@naver.com>
2023-02-09 11:09:38 +0900

    유연석 : BOJ_2447_G5_별찍기_Fix

--- 유연석/week3/BOJ_2447_G5_별찍기.java
+++ 유연석/week3/BOJ_2447_G5_별찍기.java
@@ -8,61 +8,71 @@ import java.io.OutputStreamWriter;
 
 public class BOJ_2447_G5_별찍기 {
 	static char[][] map;
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		// input
-		int N = Integer.parseInt(br.readLine());
-		br.close();
+		
+		//input
+		int N = Integer.parseInt(br.readLine());		
 		map = new char[N][N];
-
-		// solving
-		stamp(N, 0, 0);
-
-		// output
-		for (int i = 0; i < N; i++) {
-			bw.write(String.copyValueOf(map[i]) + "\n");
+		
+		//solving
+		/**
+		 * 시작 크기는 N
+		 * 시작할 때 나를 호출한 더 큰 사각형은 없으니까 내 시작 인덱스는 (0, 0)
+		 */
+		stamp(N, 0, 0);	
+		
+		//output
+		for (int i=0; i<N; i++) {
+			bw.write(String.copyValueOf(map[i])+"\n");
 		}
 		bw.close();
+		br.close();
 	}
-
-	private static void stamp(int n, int x, int y) {
-		// n*n크기의 공간을 3*3 등분해서 divide
-		int partSize = n / 3;
-		if (n == 1)
-			return;
-
-		// 작은 공간 (9개 중 한 덩어리)
-		for (int k = 0; k < 9; k++) {
-			// x, y는 내가 호출된 지역 (블록의 인덱스)
-			// j, i는 이번에 내가 가야될 지역
-			int j = k % 3;
-			int i = k / 3;
-			// 가운데 일 때 whitespace 출력
-			if (k == 4) {
+	
+	/**
+	 * 나를 전체 n*n 정사각형으로 보고, 9등분을 해서 작은 부분을 해결
+	 * @param n : 현재의 가로 길이 (시작은 N),
+	 * 
+	 * @param x : 내가 호출된 인덱스 (나를 호출한 이전의 큰 정사각형에서 내가 몇번째 였는지)
+	 * @param y
+	 */
+	private static void stamp(int n, int x, int y) {	//xy는 큰변의 시작인덱스
+		//n*n크기의 공간을 3*3 등분해서 divide
+		int partSize = n/3;
+		if (n == 1) return;
+		
+		//작은 공간 (9개 중 한 덩어리)
+		for (int k=0; k<9; k++) {
+			//j, i는 내가 9등분한 작은 블록들의 인덱스
+			int j =k%3;	//k=5 인데, 2
+			int i =k/3;	//1
+			//작은 블럭 9개 중 가운데 (1,1)은 따로 재귀 안하고 공백 찍기
+			if (k==4) {
 				stampWhite(partSize, x, y);
 				continue;
-			}
+			} 
 			// 가운데 아닐 땐,
-			// 최소 크기면 *찍고 아니면 더 작은 재귀 호출
-			if (n == 3) {
-				map[x + i][y + j] = '*';
-			} else {
-				stamp(partSize, x + partSize * j, y + partSize * i);
+			// 9등분한 작은 사각형이 별을 찍어야 할 최소 크기이면
+			// *찍고 아니면 더 작은 재귀 호출 하면서 나를 계속 작게 분할
+			if (n==3) {
+				map[x+i][y+j] = '*';
+			} else { 
+				stamp(partSize, x+partSize*j, y+partSize*i);
 			}
-
+				
 		}
 	}
-
-	// 공백 찍기
+	//공백 찍기
 	private static void stampWhite(int n, int x, int y) {
-		for (int i = n + y; i < n + y + n; i++) {
-			for (int j = n + x; j < n + x + n; j++) {
+		// 작은 블록 9개 중에 공백은 항상 (1,1)이니까 (n*i)에서 i가 생략되어 내 인덱스가 (n,n),
+		// 이전의 블록에서 나를 호출했던 내 시작 위치가 (x,y)
+		for (int i=n+y; i<n+y+n; i++) {
+			for (int j=n+x; j<n+x+n; j++) {
 				map[i][j] = ' ';
 			}
 		}
 	}
-
-}
+	
+} 
