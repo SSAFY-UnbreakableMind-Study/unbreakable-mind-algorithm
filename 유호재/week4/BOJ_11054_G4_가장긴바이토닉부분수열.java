ee997eaf5e7a5b93a10e8a3600309881ce89914d
yuhojae <ho_0214@naver.com>
2023-02-15 01:44:20 +0900

    feat: 유호재 BJ_11054_가장긴바이토닉부분수열 add

--- 유호재/week4/BOJ_11054_G4_가장긴바이토닉부분수열.java
+++ 유호재/week4/BOJ_11054_G4_가장긴바이토닉부분수열.java
@@ -1,39 +1,3 @@
-## BOJ_2447_G5_별찍기
-- DP
-- https://www.acmicpc.net/problem/11054
-
-
-
-## 풀이
-앞에서부터 자기보다 작은 값중 count값이 가장 큰 값을 체크하여 +1
-뒤에서부터 자기보다 큰 값중 count값이 가장 큰 값을 체크하여 +1
-
-~~~java
-for(int i = 1; i <=n; i++) {
-	arr[0][i] = Integer.parseInt(st.nextToken());
-	int max = 0;
-	for(int j = i-1; j >= 0; j--) {
-		if(arr[0][j] < arr[0][i]) {
-			max = Math.max(max, arr[1][j]);
-		}
-	}
-	arr[1][i] = max+1;
-}
-
-for(int i = n; i >= 1; i--) {
-	int max = 0;
-	for(int j = i+1; j <= n; j++) {
-		if(arr[0][i] > arr[0][j]) {
-			max = Math.max(max, arr[2][j]);
-		}
-	}
-	arr[2][i] = max+1;
-	answer = Math.max(answer, arr[1][i]+arr[2][i]);
-}
-~~~
-
-## 소스코드
-~~~java
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -90,12 +54,3 @@ public class BJ_11054_가장긴바이토닉부분수열 {
 	}
 
 }
-~~~
-
-
-## 결과 
-
-| 메모리  | 시간 |
-|----|----|
-| 15440 KB| 164ms|
-
